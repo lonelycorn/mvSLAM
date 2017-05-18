04b203762c9620b5ad61df0307fbbfdabf22d43e
Chengyuan Yan <yanchengyuan@gmail.com>
2017-05-18 09:29:20 -0700

    Switched to Id::Type for consistency.

--- source/visualization/data-types.hpp
+++ source/visualization/data-types.hpp
@@ -10,12 +10,12 @@ namespace mvSLAM
 {
 struct VisualizationTypes
 {
-    using CameraId = uint32_t;
+    using CameraId = Id::Type;
 
-    using PointId = uint32_t;
+    using PointId = Id::Type;
     using Point3 = Vector3Type;
 
-    using PointCloudId = uint32_t;
+    using PointCloudId = Id::Type;
     using PointCloud = std::unordered_map<PointId, Point3>;
 };
 
